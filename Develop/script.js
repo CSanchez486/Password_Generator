a0306bc124fe5d18ca21ff290fa15b2db94ec959
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-24 02:02:52 -0500

    created the if function to add to random array

--- Develop/script.js
+++ Develop/script.js
@@ -10,23 +10,24 @@ var numericSigns = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharacters = [ "!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","^","_","`","{","|","}","~"];
 
 // Variables for checkP function
-var hasSC;
-var hasNS;
 var hasUCL;
 var hasLCL;
+var hasSC;
+var hasNS;
 
 //Checks that password has correct character types 
 function checkP () {
-  hasSC = window.confirm("Cancel means No, OK means OK - click away!");
-  hasNS = window.confirm("Cancel means No, OK means OK - click away!");
   hasUCL = window.confirm("Cancel means No, OK means OK - click away!");
   hasLCL = window.confirm("Cancel means No, OK means OK - click away!");
+  hasSC = window.confirm("Cancel means No, OK means OK - click away!");
+  hasNS = window.confirm("Cancel means No, OK means OK - click away!");
 }
 
 
 // This function will determine the password length.
 // initial prompt
-function keyLength () { 
+function generatePassword() { 
+  var password = [];
   var passwordLength = prompt("How many characters does your password need to be? The length of the password can be between 8-128 characters");
 
   //prompt if cancel is clicked - exits the generator
@@ -55,132 +56,27 @@ function keyLength () {
   };
   // console.log(keyLength)
 
-// This function will confirm if uppercase letters are needed
-//initial prompt 
-function uppercaseNeeded() { 
-  uppercaseConfirm = prompt("Are uppercase letters required for your password?\n (Y / N)");
+//randomized characters will be placed in empty array
+var randomC = [];
+//count starts at 0
+var start = 0;
 
-  // prompt if text field is blank  
-  if (uppercaseConfirm === null || uppercaseConfirm === "") { 
-      alert("Choose Y or N"); 
-      uppercaseNeeded;
+//variables for if statements that will generate password
+var randomUCL;
+var randomLCL;
+var randomSC;
+var randomNS;
 
-      // allows for variations of Y to be accepted as a response
-    }else if (uppercaseConfirm === "Y" || uppercaseConfirm === "y"){
-        uppercaseConfirm = true;
-        return uppercaseConfirm
-
-        // allows for variations of N to be accepted as a response
-    }else if (uppercaseConfirm === "N" || uppercaseConfirm === "n"){ 
-        uppercaseConfirm = false;
-        return lowercaseConfirm
-        //determines if uppercase letters are needed
-    }else {
-        alert("Y or N");
-        uppercaseNeeded ()
-      }
-      return uppercaseConfirm;
+if(randomUCL) { 
+  uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+  randomC.push(randomUCL);
+  //randomUCL gets pushed up to the array
+  password.push(...uppercaseLetters);
 }
-
-// This function will determine if numbers are needed
-// initial prompt
-function numericNeeded () { 
-  numericConfirm = prompt("Are numbers required for your password?\n (Y / N)");
-
-   //prompt if text field is blank  
-  if (numericConfirm === null || numericConfirm === " " ) { 
-      alert("Choose Y or N");
-      numericNeeded;
-  
-      // allows for variations of Y to be accepted as a response
-    }else if (numericConfirm === "Y" || numericConfirm === "y" ){
-      numericConfirm = true;
-      return numericConfirm
-
-      // allows for variations of N to be accepted as a response
-    }else if (numericConfirm === "N" || numericConfirm === "n" ){
-      numericConfirm = false;
-
-      //determines if numbers are needed
-    } else {
-      alert("Y or N");
-      numericNeeded ()
-    }
-    return numericConfirm;
-}
-
- // initial prompt
-function specialNeeded () {
-  specialConfirm = prompt("Are special characters required for your password?\n (Y / N)");
-  //prompt if text field is blank  
-  if (specialConfirm === null || specialConfirm === " " ) {  
-      alert("Choose Y or N");
-      numericNeeded;
-  
-      // allows for variations of Y to be accepted as a response
-    }else if (specialConfirm === "Y" || specialConfirm === "y" ){
-      numericConfirm = true;
-      return numericConfirm
-
-      // allows for variations of N to be accepted as a response
-    }else if (specialConfirm === "N" || specialConfirm === "n" ){
-      numericConfirm = false;
-
-      //determines if special characters are needed
-    } else {
-      alert("Y or N");
-      specialNeeded ()
-    }
-    return specialConfirm;
-}
-
-function generatePassword() {
-  keyLength;
-  // console.log(passwordLength);
-  uppercaseNeeded;
-  // console.log(uppercaseConfirm);
-  numericNeeded;
-  // console.log(numericConfirm);
-  specialNeeded;
-  // console.log(specialConfirm)
-  }
-
-  // Password formula
-if (uppercaseNeeded && numericNeeded && specialNeeded){ // password generated with letters (upper & lower), numbers, and special letters of the password
-  lowercaseLetters += uppercaseLetters + numericSigns + specialCharacters;
-  
-}else if(uppercaseNeeded && numericNeeded) // password generated for  only letter (upper & lowers) and numbers
-  {lowercaseLetters += uppercaseLetters + numericSigns;
-
-}else if(uppercaseNeeded && specialNeeded) //password generated for only letter (upper & lowers) and special characters
-  {lowercaseLetters += uppercaseLetters = specialCharacters;
-
-}else if (specialNeeded && numericNeeded)  //password generated for only special characters and numbers 
-  {specialCharacters && numericSigns;
-
-}else if(uppercaseNeeded)  //password with only letters
-  {lowercaseLetters += uppercaseLetters;
-
-}else if(numericNeeded) // password with only lowercase letters & numbers
-  {lowercaseLetters += numericSigns;
-
-}else if(specialNeeded)  //password with only lowercase and special characters
-  {lowercaseLetters += specialCharacters;
-
-  var lowercaseNeeded
-
-}else if(lowercaseNeeded)  //password if only lowercase letters are needed
-  {lowercaseNeeded === lowercaseLetters;
-
-    for(var i=0; i < passwordLength; i++){
-      
-    }
-}
-
-
+ 
 
 //   return String.charAt (Math.floor(Math.random() * specialCharacters.length);
-// }
+// 
 
 var password
 // Write password to the #password input
@@ -189,6 +85,8 @@ var password = generatePassword();
 var passwordText = document.querySelector("#password");
 passwordText.value = password;
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
